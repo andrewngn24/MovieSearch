8b1dea1404fff594b49e33a861f8e99885cb54fd
Hung Anh Nguyen <hunganhngn247@gmail.com>
2026-05-08 13:53:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   root: 'html',
   build: {
-    outDir: '../docs',
+    outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
